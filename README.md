352cb05fae58574497f2997dcea6cad8ba8be8fc
Parker Mei <parkermeicollege@gmail.com>
2024-11-21 10:14:47 -0500

    Updated to read the raw files and HDR files.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sUAS Calibration Scripts
 Collection of scripts for calibrating FLIR camera on IMGS MX-2 platform.
+
+Run
+```
+pip install -e .
+```
+to set up the project   
--- tests/camera_test.py
+++ tests/camera_test.py
@@ -1,4 +1,6 @@
 from src.cameras import Camera
+from matplotlib import pyplot as plt
+
 
 filepath = (
     "/Users/parkermei/Projects/Github/uas-thermal-cal/data/images/Tam Data/raw_0.hdr"
@@ -10,3 +12,10 @@ img = Tam.read(filepath)
 
 print(img.shape)
 print(img)
+
+test = img[:,:,500]
+
+plt.figure(figsize=(8, 6))
+plt.imshow(test)
+plt.colorbar()
+plt.show()

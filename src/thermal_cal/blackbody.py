Fixed bugs relating to Blackbody Units and Calibration. Updated the Read Me

src/thermal_cal/blackbody.py
@@ -29,7 +29,7 @@ class Blackbody:
         """
         temperature = temperature + 273.15  # Convert temperature to Kelvin
         wavelength = wavelength * 1e-6  # Convert micrometers to meters
-        c1 = 2 * np.pi * self.h * self.c**2
+        c1 = 2 * np.pi * self.h * (self.c**2)
         c2 = self.h * self.c / (self.k * temperature * wavelength)
         numerator = c1
         denominator = wavelength**5 * (np.exp(c2) - 1)
@@ -48,11 +48,14 @@ class Blackbody:
         """
         temperature = temperature + 273.15  # Convert temperature to Kelvin
         wavelength = wavelength * 1e-6  # Convert micrometers to meters
-        c1 = 2 * self.h * self.c**2
+        c1 = 2 * np.pi * self.h * (self.c**2)
         c2 = self.h * self.c / (self.k * temperature * wavelength)
         numerator = c1
         denominator = wavelength**5 * (np.exp(c2) - 1)
-        return numerator / denominator * 10e-7
+
+        radiance = (numerator / denominator * 10e-7) / np.pi
+
+        return radiance
 
     def band_radiance(self, rsr_path, temperature):
         """

src/thermal_cal/cal_utils.py
@@ -32,7 +32,7 @@ class CalUtils:
         comb_img = np.concatenate(images, axis=2)
 
         return comb_img
-    
+
     def bad_pixel_map(self, image) -> np.ndarray:
         """
         Create a bad pixel map from an image.
@@ -43,10 +43,9 @@ class CalUtils:
         Returns:
             np.ndarray: Bad pixel map
         """
-
+        # IGNORE ALL THIS IS WRONG!!!!!!!!!!!
         # Calculate the standard deviation of the image
         std_dev = np.std(image, axis=2)
-
 
         # Threshold the standard deviation to create a bad pixel map
         bad_pixel_map = std_dev > 640
@@ -54,3 +53,22 @@ class CalUtils:
         bad_pixel_map = bad_pixel_map.astype(int)
 
         return bad_pixel_map
+
+    def quantization(self, image) -> np.ndarray:
+        """
+        Quantize an image to 8-bit.
+
+        Args:
+            image (np.ndarray): Image array
+
+        Returns:
+            np.ndarray: Quantized image
+        """
+
+        # Normalize the image
+        image = (image - np.min(image)) / (np.max(image) - np.min(image))
+
+        # Quantize the image to 8-bit
+        quantized_image = (image * 255).astype(np.uint8)
+
+        return quantized_image

tests/camera_test.py
@@ -17,10 +17,9 @@ hist_img = np.histogram(img)
 
 print(hist_img)
 
-plt.hist(hist_img, bins= 'auto')  
+plt.hist(hist_img, bins="auto")
 plt.title("Histogram")
 plt.savefig("Histogram2.png")
-
 
 
 plt.figure(figsize=(8, 6))
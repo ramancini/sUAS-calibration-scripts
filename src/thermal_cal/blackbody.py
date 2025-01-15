Fixed the Blackbody Class, the units make sense now.

src/thermal_cal/blackbody.py
@@ -21,12 +21,13 @@ class Blackbody:
 
         Parameters:
         wavelength: Wavelength in micrometers.
-        temperature: Temperature in Kelvin.
+        temperature: Temperature in Celsius.
 
         Returns:
         Exitance in W/m^2/sr/µm.
         """
-        wavelength = wavelength * 1e-6  # Convert wavelength to meters
+        temperature = temperature + 273.15  # Convert temperature to Kelvin
+        wavelength = wavelength * 1e-6  # Convert micrometers to meters
         c1 = 2 * np.pi * self.h * self.c**2
         c2 = self.h * self.c / (self.k * temperature * wavelength)
         numerator = c1
@@ -39,12 +40,13 @@ class Blackbody:
 
         Parameters:
         wavelength: Wavelength in micrometers.
-        temperature: Temperature in Kelvin.
+        temperature: Temperature in Celsius.
 
         Returns:
         Radiance in W/m^2/sr/µm.
         """
-        wavelength = wavelength * 1e-6  # Convert wavelength to meters
+        temperature = temperature + 273.15  # Convert temperature to Kelvin
+        wavelength = wavelength * 1e-6  # Convert micrometers to meters
         c1 = 2 * self.h * self.c**2
         c2 = self.h * self.c / (self.k * temperature * wavelength)
         numerator = c1
@@ -63,7 +65,6 @@ class Blackbody:
         Returns:
         Band radiance for pixel element.
         """
-        temp = temperature + 273.15  # Convert temperature to Kelvin
-        band_radiance = np.multiply(self.planck_radiance(wavelengths, temp), rsr)
+        band_radiance = np.multiply(self.planck_radiance(wavelengths, temperature), rsr)
         total_radiance = integrate.trapezoid(band_radiance, wavelengths)
         return total_radiance

src/thermal_cal/cal_utils.py
@@ -6,7 +6,7 @@ class CalUtils:
     def __init__(self):
         pass
 
-    def combine(self, file_list):
+    def combine(self, file_list) -> np.ndarray:
         """
         Combine images from a list of file paths into a single numpy array.
 
@@ -16,6 +16,7 @@ class CalUtils:
         Returns:
             np.ndarray: Combined image array
         """
+
         # Initialize an empty list to store images
         images = []
 
@@ -27,7 +28,7 @@ class CalUtils:
             img = reader.read(path)
             images.append(img)
 
-        # Stack images along a new axis
+        # Stack images along the third axis
         comb_img = np.concatenate(images, axis=2)
 
         return comb_img